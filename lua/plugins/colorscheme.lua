efa48e5e3cb3bf1279f5ad650f400fcf693da32d
triet.truong <triet.truong@zoi.tech>
2024-05-15 16:38:55 +0700

    Delete fine-cmdline & changed colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,53 @@
 return {
 	{
-		"ful1e5/onedark.nvim",
+		-- "ful1e5/onedark.nvim",
+		-- lazy = false,
+		-- priority = 1000,
+		-- config = function()
+		-- 	local onedark = require("onedark")
+		-- 	onedark.setup({
+		-- 		transparent = true,
+		-- 	})
+		-- end,
+	},
+	{
+		-- "projekt0n/github-nvim-theme",
+		-- lazy = false,
+		-- priority = 1000,
+		-- config = function()
+		-- 	local github = require("github-theme")
+		-- 	github.setup({})
+		-- 	vim.cmd("colorscheme github_dark_tritanopia")
+		-- end,
+	},
+	{
+		"scottmckendry/cyberdream.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			local onedark = require("onedark")
-			onedark.setup({
+			require("cyberdream").setup({
+				-- Recommended - see "Configuring" below for more config options
 				transparent = true,
+				italic_comments = true,
+				hide_fillchars = true,
+				borderless_telescope = true,
+				terminal_colors = true,
 			})
+			vim.cmd("colorscheme cyberdream") -- set the colorscheme
 		end,
+	},
+	{
+		-- "neanias/everforest-nvim",
+		-- lazy = false,
+		-- priority = 1000,
+		-- config = function()
+		-- 	local everforest = require("everforest")
+		-- 	everforest.setup({
+		-- 		transparent_background_level = 50,
+		-- 	})
+		--
+		-- 	everforest.load()
+		-- end,
 	},
 	{
 		-- "Mofiqul/vscode.nvim",
--- lua/plugins/fine-cmdline.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"VonHeikemen/fine-cmdline.nvim",
-	dependencies = {
-		{ "MunifTanjim/nui.nvim" },
-	},
-	keys = {
-		{ ";", "<cmd>FineCmdline<CR>", desc = "Open fine cmdline", silent = true },
-	},
-}
